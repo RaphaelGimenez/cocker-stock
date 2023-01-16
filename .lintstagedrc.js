569a909d2da06d5a4289ad8decb59954f07c6bec
Raphael <contact@raphaelgimenez.fr>
2023-01-16 11:57:24 +0100

    fix(lintstaged): change scripts name

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,10 @@
 const config = {
   'apps/stock-dashboard/**/*.{js,jsx,ts,tsx}': (filenames) =>
-    `pnpm lint:app --fix --lintFilePatterns=${filenames}`,
+    `pnpm lint:stock-dashboard --fix --lintFilePatterns=${filenames}`,
   'apps/stock-dashboard-e2e/**/*.{js,jsx,ts,tsx}': (filenames) =>
-    `pnpm lint:app-e2e --fix --lintFilePatterns=${filenames}`,
+    `pnpm lint:stock-dashboard-e2e --fix --lintFilePatterns=${filenames}`,
   'apps/server/**/*.{js,jsx,ts,tsx}': (filenames) =>
-    `pnpm lint:app-e2e --fix --lintFilePatterns=${filenames}`,
+    `pnpm lint:server-e2e --fix --lintFilePatterns=${filenames}`,
 };
 
 module.exports = config;
