35d5d77974ffacf4df8f8f7012e8810966707744
Raphael <contact@raphaelgimenez.fr>
2023-01-18 21:27:59 +0100

    feat(sd/login): display auth form

--- apps/stock-dashboard/pages/login/index.tsx
+++ apps/stock-dashboard/pages/login/index.tsx
@@ -1,3 +1,5 @@
+import { AuthForm } from '@cocker-stock/stock-dashboard/feature-auth';
+
 /* eslint-disable-next-line */
 export interface LoginProps {}
 
@@ -5,6 +7,7 @@ export function Login(props: LoginProps) {
   return (
     <div>
       <h1>Welcome to Login!</h1>
+      <AuthForm />
     </div>
   );
 }
