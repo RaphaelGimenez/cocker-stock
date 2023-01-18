0a938d2767cb5f45bb3191ff878301ce9df831cb
Raphael <contact@raphaelgimenez.fr>
2023-01-18 18:08:35 +0100

    test(sd-e2e): ref signup to expect supabase ui

--- apps/stock-dashboard-e2e/src/e2e/signup.cy.js
+++ apps/stock-dashboard-e2e/src/e2e/signup.cy.js
@@ -8,47 +8,24 @@ const supabase = createClient(
 describe('Signup page', () => {
   beforeEach(() => cy.visit('/'));
   it('should let user sign up with valid credentials', () => {
-    cy.intercept('POST', '/signup').as('signup');
-
-    const existingUser = {
-      email: faker.internet.exampleEmail(),
-      password: faker.internet.password(),
-    };
-    const validUser = {
+    const user = {
       email: faker.internet.exampleEmail(),
       password: faker.internet.password(),
     };
 
-    supabase.auth.admin.createUser({
-      email: existingUser.email,
-      password: existingUser.password,
-      email_confirm: true, // to auto confirm email
+    cy.wrap(supabase.auth.admin.listUsers()).then((res) => {
+      cy.findByRole('link', { name: /créer un compte/i }).click();
+
+      cy.findByLabelText(/email/i).type(user.email);
+      cy.findByLabelText(/mot de passe/i).type(user.password);
+      cy.findByRole('button', { name: /créer un compte/i }).click();
+      cy.findByText(/un email de confirmation vous a été envoyé/i).should(
+        'exist'
+      );
+
+      cy.wrap(supabase.auth.admin.listUsers())
+        .its('data.users')
+        .should('have.length', res.data.users.length + 1);
     });
-
-    cy.findByRole('link', { name: /créer un compte/i }).click();
-
-    // test with existing email
-    cy.findByLabelText(/email/i).type(existingUser.email);
-    cy.findByLabelText(/mot de passe/i).type(validUser.password);
-    cy.findByRole('button', { name: /créer un compte/i }).click();
-    cy.wait('@signup').its('response.statusCode').should('eq', 400);
-    cy.findByText(/cet email est déjà utilisé/i).should('exist');
-
-    // test with valid email
-    cy.findByLabelText(/email/i).type(validUser.email);
-    cy.findByLabelText(/mot de passe/i).type(validUser.password);
-    cy.findByRole('button', { name: /créer un compte/i }).click();
-
-    // User should be created in supabase
-    cy.wait('@signup').then((interception) => {
-      expect(interception.response.statusCode).to.eq(200);
-      cy.wrap(supabase.auth.admin.getUserById(interception.response.body.uid))
-        .its('response.statusCode')
-        .should('eq', 200);
-    });
-
-    cy.findByText(/un email de confirmation vous a été envoyé/i).should(
-      'exist'
-    );
   });
 });
