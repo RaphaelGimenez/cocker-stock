07bf85ee4c3f1a590512d88252be3e00158e92ac
Raphael <contact@raphaelgimenez.fr>
2023-01-18 11:25:07 +0100

    fix(stock-dashboard-e2e): add supabase env

--- apps/stock-dashboard-e2e/cypress.config.ts
+++ apps/stock-dashboard-e2e/cypress.config.ts
@@ -3,4 +3,8 @@ import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
   e2e: nxE2EPreset(__dirname),
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_API_KEY: process.env.SUPABASE_API_KEY,
+  },
 });
