a08dbba88bb8ed2335e0950b887daab5ccced10c
Raphael <contact@raphaelgimenez.fr>
2023-01-18 11:34:38 +0100

    feat(sd/home): display signup button

--- apps/stock-dashboard/pages/index.tsx
+++ apps/stock-dashboard/pages/index.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@mantine/core';
+import Link from 'next/link';
+
 export function Index() {
   return (
     <div>
       <h1>Welcome to stock-dashboard!</h1>
+      <Link href="/signup">
+        <Button component="span">créer un compte</Button>
+      </Link>
     </div>
   );
 }
