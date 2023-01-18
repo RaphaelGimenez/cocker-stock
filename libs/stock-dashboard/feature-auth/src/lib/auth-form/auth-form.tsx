162d62447c72c519d9902c35903326cdedae4f72
Raphael <contact@raphaelgimenez.fr>
2023-01-18 19:38:51 +0100

    feat(sd/auth-form): change labels and set initial view to sign up

--- libs/stock-dashboard/feature-auth/src/lib/auth-form/auth-form.tsx
+++ libs/stock-dashboard/feature-auth/src/lib/auth-form/auth-form.tsx
@@ -5,7 +5,27 @@ import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
 export interface AuthFormProps {}
 
 export function AuthForm(props: AuthFormProps) {
-  return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  return (
+    <Auth
+      supabaseClient={supabase}
+      appearance={{ theme: ThemeSupa }}
+      view="sign_up"
+      localization={{
+        variables: {
+          sign_in: {
+            email_label: 'Email',
+            password_label: 'Mot de passe',
+            button_label: 'Se connecter',
+          },
+          sign_up: {
+            email_label: 'Email',
+            password_label: 'Mot de passe',
+            button_label: "S'inscrire",
+          },
+        },
+      }}
+    />
+  );
 }
 
 export default AuthForm;
