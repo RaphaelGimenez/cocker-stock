be2ec933834a429b2caba9781f276178cb77eff0
Raphael <contact@raphaelgimenez.fr>
2023-01-18 20:58:31 +0100

    refactor(sd-e2e): base test on supabase api

--- apps/stock-dashboard-e2e/src/e2e/signup.cy.js
+++ apps/stock-dashboard-e2e/src/e2e/signup.cy.js
@@ -1,31 +1,21 @@
-import { createClient } from '@supabase/supabase-js';
 import { faker } from '@faker-js/faker';
-const supabase = createClient(
-  Cypress.env('SUPABASE_URL'),
-  Cypress.env('SUPABASE_API_KEY')
-);
 
 describe('Signup page', () => {
   beforeEach(() => cy.visit('/'));
   it('should let user sign up with valid credentials', () => {
+    cy.intercept(/\/auth\/v1\/signup/).as('signup');
     const user = {
       email: faker.internet.exampleEmail(),
       password: faker.internet.password(),
     };
 
-    cy.wrap(supabase.auth.admin.listUsers()).then((res) => {
-      cy.findByRole('link', { name: /créer un compte/i }).click();
+    cy.findByRole('link', { name: /créer un compte/i }).click();
+    cy.location('pathname').should('eq', '/signup');
 
-      cy.findByLabelText(/email/i).type(user.email);
-      cy.findByLabelText(/mot de passe/i).type(user.password);
-      cy.findByRole('button', { name: /s'inscrire/i }).click();
-      cy.findByText(/un email de confirmation vous a été envoyé/i).should(
-        'exist'
-      );
+    cy.findByLabelText(/email/i).type(user.email);
+    cy.findByLabelText(/mot de passe/i).type(user.password);
+    cy.findByRole('button', { name: /s'inscrire/i }).click();
 
-      cy.wrap(supabase.auth.admin.listUsers())
-        .its('data.users')
-        .should('have.length', res.data.users.length + 1);
-    });
+    cy.wait('@signup').its('response.body.user').should('exist');
   });
 });
