74be840538a8e3be407be6f1256da41fee3fcf04
Raphael <contact@raphaelgimenez.fr>
2023-01-18 18:59:35 +0100

    feat(sd-signup): use AuthForm

--- apps/stock-dashboard/pages/signup/index.tsx
+++ apps/stock-dashboard/pages/signup/index.tsx
@@ -1,4 +1,4 @@
-import { SignupForm } from '@cocker-stock/stock-dashboard/feature-signup';
+import { AuthForm } from '@cocker-stock/stock-dashboard/feature-auth';
 
 /* eslint-disable-next-line */
 export interface SignupProps {}
@@ -7,7 +7,7 @@ export function Signup(props: SignupProps) {
   return (
     <div>
       <h1>Welcome to Signup!</h1>
-      <SignupForm />
+      <AuthForm />
     </div>
   );
 }
