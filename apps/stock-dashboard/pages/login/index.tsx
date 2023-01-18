c82e4f6910d2bf62ef4b29b03bb64d702e3e8107
Raphael <contact@raphaelgimenez.fr>
2023-01-18 21:37:04 +0100

    feat(sd/login): redirect to dashboard on user sign in

--- apps/stock-dashboard/pages/login/index.tsx
+++ apps/stock-dashboard/pages/login/index.tsx
@@ -1,9 +1,20 @@
 import { AuthForm } from '@cocker-stock/stock-dashboard/feature-auth';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useRouter } from 'next/router';
 
 /* eslint-disable-next-line */
 export interface LoginProps {}
 
 export function Login(props: LoginProps) {
+  const router = useRouter();
+  const supabaseClient = useSupabaseClient();
+
+  supabaseClient.auth.onAuthStateChange((event, session) => {
+    if (event === 'SIGNED_IN') {
+      router.push('/dashboard');
+    }
+  });
+
   return (
     <div>
       <h1>Welcome to Login!</h1>
