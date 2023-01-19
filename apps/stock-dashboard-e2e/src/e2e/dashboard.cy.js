d519e3cacf05e6ef643f85928d1d8da1d3ddedbb
Raphael <contact@raphaelgimenez.fr>
2023-01-19 18:52:19 +0100

    test(sd-e2e): should let user create and access an organization

--- apps/stock-dashboard-e2e/src/e2e/dashboard.cy.js
+++ apps/stock-dashboard-e2e/src/e2e/dashboard.cy.js
@@ -11,4 +11,30 @@ describe('Dashboard page', () => {
 
     cy.location('pathname').should('eq', '/login');
   });
+
+  it('should let user create and access an organization', () => {
+    cy.createUserAndLogin();
+
+    const spaceName = faker.company.name();
+    const spaceSlug = slugify(spaceName);
+
+    cy.findAllByTestId(/space-card/i).should('have.length', 0);
+
+    cy.findByRole('button', { name: /créer une organisation/i }).click();
+
+    cy.location('pathname').should('eq', '/dashboard/new-space');
+
+    cy.findByLabelText(/nom/i).type(spaceName);
+    cy.findByRole('button', { name: /créer/i }).click();
+
+    cy.location('pathname').should('eq', `/dashboard/sapces/${spaceSlug}`);
+
+    cy.visit('/dashboard');
+
+    cy.findAllByTestId(/space-card/i).should('have.length', 1);
+
+    cy.findByRole('link', { name: spaceName }).click();
+
+    cy.location('pathname').should('eq', `/dashboard/sapces/${spaceSlug}`);
+  });
 });
